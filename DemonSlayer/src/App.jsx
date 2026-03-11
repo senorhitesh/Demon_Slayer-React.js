d036cfcf576be885971ed40ce322cd258e71dc30
Hitesh Suthar <senorhitesh@gmail.com>
2026-03-11 14:11:17 +0530

    css file set-up

--- DemonSlayer/src/App.jsx
+++ DemonSlayer/src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
+import Hero from './Components/Hero'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+      <Hero/>
+    </main>
   )
 }
 
---
+++ DemonSlayer/src/Components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+      <div className='relative overflow-x-hidden h-dvh w-screen'>
+          <div id="video-frame" className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg'>
+              <div className='mask-clip-path'></div>
+              
+          </div>
+    </div>
+  )
+}
+
+export default Hero
