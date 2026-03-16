7eb4ba733c4cbfdb9fb1a1adb77b4ed39f2becdf
Hitesh Suthar <senorhitesh@gmail.com>
2026-03-16 17:50:40 +0530

    div center fixed

--- DemonSlayer/src/Components/Hero.jsx
+++ DemonSlayer/src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import hero1 from "../assets/videos/hero-1.mp4";
 import hero2 from "../assets/videos/hero-2.mp4";
 
 const Hero = () => {
-  const [currentIndex, setCurrentIndex] = useState( 1);
+  const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClicked, sethasClicked] = useState(false);
   const [isLoading, setisLoading] = useState(true)
   const [loadedVds, setloadedVds] = useState(0);
@@ -25,20 +25,24 @@ const Hero = () => {
   const getVideoSource = (index) => `/videos/hero-${index}.mp4`;
 
   return (
-      <div className='relative overflow-x-hidden h-ful w-screen '>
-          <div id="video-frame" className='relative z-10 h-dvh w-fit overflow-hidden rounded-lg'>
-              <div className='clip-path-[polygon(0_0,100%_0,100%_100%,0_100%)] cursor-pointer  overflow-hidden rounded-lg'></div>
-        <div className='overflow-hidden w-fit border-4 rounded-md group hover:opacity-100 transition-opacity duration-500 ease-linear'
-          onClick={() => {
-                 handleMiniVdClick()
-                        }} >
+    // Main Content
+    <div className='relative h-full w-screen '>
+      {/* Bg-Video */}
+      <div id="video-frame" className='h-screen flex justify-center items-center'>
+        
+        <div className='clip-path-[polygon(0_0,100%_0,100%_100%,0_100%)] cursor-pointer  overflow-hidden rounded-lg'></div>
+                {/* Center-Video */}
+                <div className='overflow-hidden  w-fit group rounded-md group hover:opacity-100 transition-opacity duration-200 ease-linear'
+                         onClick={() => {
+                         handleMiniVdClick()
+                          }} >
                          <video
                            ref={videoRef}
                            src={videos[currentIndex]}
                            muted
                            loop
                            id="current-video"
-                           className='object-center absolute top-1/2   object-cover size-54'
+                           className='object-center scale-100 group-hover:scale-120 transition duration-75 object-cover size-64'
                            onLoadedData={handleVideoLoaded}
                          />
                 </div>
@@ -48,3 +52,25 @@ const Hero = () => {
 }
 
 export default Hero
+
+
+
+    //   <div className='relative overflow-x-hidden h-ful w-screen '>
+    //       <div id="video-frame" className='relative z-10 h-dvh w-fit overflow-hidden rounded-lg'>
+    //           <div className='clip-path-[polygon(0_0,100%_0,100%_100%,0_100%)] cursor-pointer  overflow-hidden rounded-lg'></div>
+    //     <div className='overflow-hidden w-fit border-4 rounded-md group hover:opacity-100 transition-opacity duration-500 ease-linear'
+    //       onClick={() => {
+    //              handleMiniVdClick()
+    //                     }} >
+    //                      <video
+    //                        ref={videoRef}
+    //                        src={videos[currentIndex]}
+    //                        muted
+    //                        loop
+    //                        id="current-video"
+    //                        className='object-center absolute top-1/2   object-cover size-54'
+    //                        onLoadedData={handleVideoLoaded}
+    //                      />
+    //             </div>
+    //       </div>
+    // </div>
